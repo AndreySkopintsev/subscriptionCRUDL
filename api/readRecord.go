a0af4181b9238eb048ddd39434138d5b2cee6875
Andrew <sanchezzvilapandozz@gmail.com>
2025-07-22 13:18:11 +0300

    added obtaining of recordId params

--- api/readRecord.go
+++ api/readRecord.go
@@ -1,7 +1,15 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	recordIdKey = "recordId"
+)
 
 func ReadRecordHandler(w http.ResponseWriter, r *http.Request) {
-
+	recordId := r.URL.Query().Get(recordIdKey)
+	fmt.Fprintf(w, "RecordID obtained=%s\n", recordId)
 }
--- api/updateRecord.go
+++ api/updateRecord.go
@@ -1,7 +1,11 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func UpdateRecordHandler(w http.ResponseWriter, r *http.Request) {
-
+	recordId := r.URL.Query().Get(recordIdKey)
+	fmt.Fprintf(w, "RecordID obtained=%s\n", recordId)
 }
